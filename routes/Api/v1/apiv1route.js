ffca502fa9a7e0b60b6ef5b6157c6ae300a335eb
Paul Tijero <paultijero@gmail.com>
2020-10-08 12:57:53 -0500

    Terminando

--- routes/Api/v1/apiv1route.js
+++ routes/Api/v1/apiv1route.js
@@ -4,6 +4,6 @@ const app = express();
 const courseRoutes = require('./cursos/courseRoute');
 
 //  dominio.../api/v1/course/
-app.use('/',courseRoutes);
+app.use('/course',courseRoutes);
 
 module.exports = app;
--- routes/Api/v1/cursos/courseRoute.js
+++ routes/Api/v1/cursos/courseRoute.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
 
-var payments = require('../../../../data');
+var enrollments = require('../../../../data');
+const { route } = require('../apiv1route');
 
 // [POST] dominio.../api/v1/log/add
-router.post('/payments',(req,res)=>{
-    
-    var payment = {idCourse: req.body.idCourse,idStudent: req.body.idStudent};
-    payments.push(payment);
-    
-    res.send(payments);
+router.post('/enrollment',(req,res)=>{
+
+    var options = {
+    'method': 'POST',
+    'url': 'http://localhost:4000/api/v1/course/validPayment',
+    'form': {
+        'idCourse': req.body.idCourse,
+        'idStudent': req.body.idStudent
+    }
+    };
+
+    request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+    if(response.body == "true" || response.body == true || response.body == 'true'){
+        var enrollment = {idCourse: req.body.idCourse,idStudent: req.body.idStudent};
+        enrollments.push(enrollment);
+        console.log(enrollments);
+        res.send(true);
+    }else{
+        res.send(false);
+    }
+    });
+
+});
+
+router.get('/enrollment/:idStudent',(req,res)=>{
+    var courses = enrollments.find(enrollment=>enrollment.idStudent== req.body.idStudent);
+    return res.send(courses);
 })
 
 module.exports = router;
